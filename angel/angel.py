314ff46015121e67f3ac83ed6b9006d8662ee75a
Jaren Glover <jaren.g@gmail.com>
2015-04-21 03:49:26 -0400

    fix format for calls + get_tag_jobs working

--- angel/angel.py
+++ angel/angel.py
@@ -49,13 +49,11 @@ _USERS_BATCH = '{c_api}/{api}/users/batch?ids={ids}'#?access_token={at}'
 _S_SEARCH = '{c_api}/{api}/search?query={query}'
 _SLUG_SEARCH = '{c_api}/{api}/search/slugs?query={slug}'
 _COM = '{c_api}/{api}/comments?commentable_type={ct}&commentable_id={id_}'
-
-# _JOBS = '{c_api}/{api}/jobs?page={pg}'
-# needs to be tested once API is back up
+#_JOBS = '{c_api}/{api}/jobs?page={pg}'
 _JOBS = '{c_api}/{api}/jobs?page={pg}&access_token={at}' # updated to include access token
 _JOBS_ID = '{c_api}/{api}/jobs/{id_}'
 _STARTUP_ID_JOBS = '{c_api}/{api}/startups/{id_}/jobs'
-_TAG_ID_JOBS = '{c_api}/{api}/tags/{id_}/jobs'
+_TAG_ID_JOBS = '{c_api}/{api}/tags/{id_}/jobs?page={pg}&access_token={at}'
 _LIKES = '{c_api}/{api}/likes?likable_type={lt}&likable_id={li}'
 _MESSAGES = '{c_api}/{api}/messages?access_token={at}'
 _MESSAGES_THREAD = '{c_api}/{api}/messages/{id_}?access_token={at}'
@@ -136,12 +134,12 @@ class AngelList(object):
                                                         id_=id_,
                                                         at=self.access_token))
 
-  def get_tag_jobs(self,page =1, id_):
+  def get_tag_jobs(self,id_, page=1):
     url = _TAG_ID_JOBS.format(c_api=_C_API_BEGINNING,
                                                     api=_API_VERSION,
-                                                    id_=id_,
-                                                    pg=page,
-                                                    at=self.access_token)
+                                                     pg=page,
+                                                     id_=id_,
+                                                     at=self.access_token)
     return _get_request(url)
 
 
